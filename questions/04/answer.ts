baa7af8e802d0ef0520760dff9f07063589f85fd
noahplazier <noahplz@hotmail.nl>
2022-12-06 01:24:05 +0100

    q4 pt2

--- questions/04/answer.ts
+++ questions/04/answer.ts
@@ -10,9 +10,15 @@ const pairContainedByOtherPair = (l: Array<number>, r: Array<number>): boolean =
 	return l[0] <= r[0] && l[1] >= r[1] || r[0] <= l[0] && r[1] >= l[1]
 }
 
+const pairsOverlapPartly = (l: Array<number>, r: Array<number>): boolean => {
+	const contained = pairContainedByOtherPair(l, r)
+	return contained || l[0] <= r[0] && l[1] >= r[0] || r[0] <= l[0] && r[1] >= l[0]
+}
+
 // Part 1
-const totalContainedPairs = pairs.map(x => pairContainedByOtherPair(x[0], x[1])).filter(x => x).length
+const totalContainedPairs = pairs.filter(x => pairContainedByOtherPair(x[0], x[1])).length
 console.log(totalContainedPairs)
 
 // Part 2
-
+const totalOverlappingPairs = pairs.filter(x => pairsOverlapPartly(x[0], x[1])).length
+console.log(totalOverlappingPairs)
